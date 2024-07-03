79106daded763ab7f252093ba42b4f795e457b16
aashishrawte1 <aashish@jklabs.ca>
2024-07-03 18:27:33 +0530

    social header udpated

--- src/components/SocialHeader.jsx
+++ src/components/SocialHeader.jsx
@@ -1,17 +1,21 @@
+import './SocialHeader.css';
+
 export const SocialHeader = () => {
-    const styles = {
-        main: {
-          backgroundColor: "#f1f1f1",
-          width: "100%",
-        }
-    };
     return (
         <header>
-            <div className="social" style={styles.main}>
-                <img height="20" width="20" alt="linkedin" src={require("../assets/images/linkedin.png")}/>
-                <img height="20" width="20" alt="instagram" src={require("../assets/images/instagram.png")}/>
-                <img height="20" width="20" alt="twitter" src={require("../assets/images/twitter.png")}/>
-                <img height="20" width="20" alt="github" src={require("../assets/images/github.png")}/>
+            <div className="social-icons">
+                <a href="https://www.linkedin.com/in/ankita-urkude-a0742b214/" className="social-icon">
+                    <img height="20" width="20" alt="linkedin" src={require("../assets/images/linkedin.png")}/>
+                </a>
+                <a href="https://www.instagram.com/ank8434/" className="social-icon">
+                    <img height="20" width="20" alt="instagram" src={require("../assets/images/instagram.png")}/>
+                </a>
+                <a href="https://x.com/AUrkude49194" className="social-icon">
+                    <img height="20" width="20" alt="twitter" src={require("../assets/images/twitter.png")}/>
+                </a>
+                <a href="https://github.com/Anku-U" className="social-icon">
+                    <img height="20" width="20" alt="github" src={require("../assets/images/github.png")}/>
+                </a>
             </div>
         </header>
     )
