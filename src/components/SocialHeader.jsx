7294a671c093873c28c2ce4b0f988e6ee3e93a3a
aashishrawte1 <aashish@jklabs.ca>
2024-07-04 20:40:27 +0530

    target blank added on anchor tag for opening social links on new tab

--- src/components/SocialHeader.jsx
+++ src/components/SocialHeader.jsx
@@ -4,16 +4,16 @@ export const SocialHeader = () => {
     return (
         <header>
             <div className="social-icons">
-                <a href="https://www.linkedin.com/in/ankita-urkude-a0742b214/" className="social-icon">
+                <a href="https://www.linkedin.com/in/ankita-urkude-a0742b214/" className="social-icon" rel="noreferrer" target='_blank'>
                     <img height="20" width="20" alt="linkedin" src={require("../assets/images/linkedin.png")}/>
                 </a>
-                <a href="https://www.instagram.com/ank8434/" className="social-icon">
+                <a href="https://www.instagram.com/ank8434/" className="social-icon"  rel="noreferrer" target='_blank'>
                     <img height="20" width="20" alt="instagram" src={require("../assets/images/instagram.png")}/>
                 </a>
-                <a href="https://x.com/AUrkude49194" className="social-icon">
+                <a href="https://x.com/AUrkude49194" className="social-icon"  rel="noreferrer" target='_blank'>
                     <img height="20" width="20" alt="twitter" src={require("../assets/images/twitter.png")}/>
                 </a>
-                <a href="https://github.com/Anku-U" className="social-icon">
+                <a href="https://github.com/Anku-U" className="social-icon"  rel="noreferrer" target='_blank'>
                     <img height="20" width="20" alt="github" src={require("../assets/images/github.png")}/>
                 </a>
             </div>
